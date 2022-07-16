559777ad79886f35aeca019d01bb5e3453339b4e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-16 17:21:19 +0530

    update: scanner.py

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@
 
 import sys
 import socket
-from datetime import datetime as time
+from datetime import datetime
 
 # Define our target
 target = ' '
@@ -16,20 +16,24 @@ else:
     print("Syntax: python3 scanner.py <ip>")
     print("Example: python3 scanner.py 192.168.0.1")
 
+
+# Check the date and time the scan was started.
+time1 = datetime.now()
+
 # Pretty banner
 print("-" * 50)
 print("Scanning target: " + target)
-print("Time started: " + str(time.now()))
+print("Time started: " + str(time1))
 print("-" * 50)
 
 try:
     for port in range(50, 85):  # 1, 65535
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port))
+        result = sock.connect_ex((target, port))
         if result == 0:
-            print("Port is {} open".format(port))
-        s.close()
+            print("Port {}:   open".format(port))
+        sock.close()
 
 except KeyboardInterrupt:
     print("\nExiting scanner.")
@@ -42,3 +46,10 @@ except socket.gaierror:
 except socket.error:
     print("Could not connect to server/ip.")
     sys.exit()
+
+
+# Checking the time again
+time2 = datetime.now()
+
+total_time_taken = time2 - time1
+print("Scanning completed in {} ".format(total_time_taken))
