d0ba4a57850d37e5e2978445a09150fd2550643d
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-16 19:16:26 +0530

    feat: verbose mode (#12) (#13)
    * update: scanner.py
    
    * Update .gitignore
    
    * feat: verbose mode (#12)
    
    Features:
    - verbose mode
    - counter
    - total open ports
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # port-scanner
 A fast port scanner written in python with a focus on reliability and simplicity. 
 
+Port specific:
+Syntax: `python3 scanner.py <ip> <port_start> <port_end>`
+![port_specific](https://user-images.githubusercontent.com/44284877/179356857-4676e09e-48ac-4cb8-96e3-2fa910a15e9a.gif)
 
-Syntax: `python3 scanner.py <ip>`
-
-Example: `python3 scanner/py 192.168.0.2`
--- scanner.py
+++ scanner.py
@@ -1,35 +1,63 @@
 #!/bin/python3
-
+import subprocess
 import sys
 import socket
-from datetime import datetime as time
+from datetime import datetime
+
+# Blank your screen
+# subprocess.call('clear', shell=True)
 
 # Define our target
 target = ' '
+start_port = ' '
+last_post = ' '
+count = 0
+open_ports = []
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 4 or 5:
     # Translate hostname to IPv4
     target = socket.gethostbyname(sys.argv[1])
+    start_port = int(sys.argv[2])
+    last_post = int(sys.argv[3])
 
 else:
     print("Invalid amount of arguments.")
-    print("Syntax: python3 scanner.py <ip>")
-    print("Example: python3 scanner.py 192.168.0.1")
+    print("Syntax: python3 scanner.py <ip> <start_port> <last_port>")
+    print("Example: python3 scanner.py 192.168.0.1 0 8888")
+
+# Check the date and time the scan was started.
+time1 = datetime.now()
 
 # Pretty banner
 print("-" * 50)
 print("Scanning target: " + target)
-print("Time started: " + str(time.now()))
+print("Time started: " + str(time1))
 print("-" * 50)
 
 try:
-    for port in range(50, 85):  # 1, 65535
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for port in range(start_port, last_post):  # 1, 65535
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print("Port is {} open".format(port))
-        s.close()
+        result = sock.connect_ex((target, port))
+        if "-v" in sys.argv:
+            if result == 0:
+                print("Port {}: open".format(port))
+                count = count + 1
+                open_ports.append(port)
+
+            else:
+                print("Port {}: close".format(port))
+        else:
+            if result == 0:
+                print("Port {}: open".format(port))
+                count = count + 1
+                open_ports.append(port)
+
+        sock.close()
+
+    print("-" * 50)
+    print("Open ports: {}".format(open_ports))
+    print("Total open ports: {}".format(count))
 
 except KeyboardInterrupt:
     print("\nExiting scanner.")
@@ -42,3 +70,9 @@ except socket.gaierror:
 except socket.error:
     print("Could not connect to server/ip.")
     sys.exit()
+
+# Checking the time again
+time2 = datetime.now()
+
+total_time_taken = time2 - time1
+print("Scanning completed in {} ".format(total_time_taken))
